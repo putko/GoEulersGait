79f16213e55cf81eb123749776751b684736b767
putko <e170595@metu.edu.tr>
2021-01-25 13:17:03 +0100

    Improvements in Problem1

--- Problem1/solution.go
+++ Problem1/solution.go
@@ -1,16 +1,18 @@
 package main
 import "fmt"
-func sum(n int) int {
+func sumOfAllNumbersTillN(n int) int {
     return n * (n+1) / 2
 }
 
 func main() {
-	var caseCount = 0
+    var caseCount = 0
     fmt.Scanln(&caseCount) 
-	for i := 0; i < caseCount; i++ {
-		var limit int
-		fmt.Scanln(&limit)
-		limit -= 1
-		fmt.Println(sum(limit/3)*3 + sum(limit/5)*5 - sum(limit/15)*15)
-	}
+    for i := 0; i < caseCount; i++ {
+        var limit int
+        fmt.Scanln(&limit)
+        limit -= 1
+        // Sum all numbers till limit/3 and then multiple it with 3 to get sum of numbers divided by 3. Add the amount you found for 5 then substract result of 15 because it would be dublicate.
+        var result = sumOfAllNumbersTillN(limit/3)*3 + sumOfAllNumbersTillN(limit/5)*5 - sumOfAllNumbersTillN(limit/15)*15 
+        fmt.Println(result)
+    }
 }
